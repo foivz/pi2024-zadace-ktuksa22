11dba5ef4e0cc2d50c8458abd7af5e529627933c
Karlo Tukša <ktuksa22@foi.hr>
2024-06-13 22:32:39 +0200

    Baza se otvara

--- Software/Treca_Zadaca/Treca_Zadaca/Osobni_nalaz.Designer.cs
+++ Software/Treca_Zadaca/Treca_Zadaca/Osobni_nalaz.Designer.cs
@@ -31,6 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Osobni_nalaz));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -39,7 +42,7 @@
             // 
             this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(83, 137);
+            this.dataGridView1.Location = new System.Drawing.Point(78, 130);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(825, 334);
             this.dataGridView1.TabIndex = 0;
@@ -47,11 +50,37 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Treca_Zadaca.Properties.Resources.Logo;
-            this.pictureBox1.Location = new System.Drawing.Point(454, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(432, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(97, 98);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(828, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(78, 480);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(191, 20);
+            this.textBoxSearch.TabIndex = 3;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(300, 480);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(113, 20);
+            this.buttonSearch.TabIndex = 4;
+            this.buttonSearch.Text = "Pretraži";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
             // 
             // Osobni_nalaz
             // 
@@ -59,6 +88,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Khaki;
             this.ClientSize = new System.Drawing.Size(984, 551);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -72,6 +104,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,6 +112,9 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearch;
     }
 }
 
--- Software/Treca_Zadaca/Treca_Zadaca/Osobni_nalaz.cs
+++ Software/Treca_Zadaca/Treca_Zadaca/Osobni_nalaz.cs
@@ -13,7 +13,6 @@ namespace Treca_Zadaca
 {
     public partial class Osobni_nalaz : Form
     {
-        string connectionString = "Server=31.147.206.65;Database=PI2324_ktuksa22_DB;User Id=PI2324_ktuksa22_User;Password=WLV7_OMG";
         public Osobni_nalaz()
         {
             InitializeComponent();
@@ -21,7 +20,51 @@ namespace Treca_Zadaca
 
         private void Osobni_nalaz_Load(object sender, EventArgs e)
         {
-
+            // Poziv funkcije za popunjavanje DataGridView-a
+            FillDataGridView();
         }
+
+        private void FillDataGridView(string searchTerm = "")
+        {
+            
+            string connectionString = "Server=31.147.206.65;Database=PI2324_ktuksa22_DB;User Id=PI2324_ktuksa22_User;Password=WLV7_OMG;";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    // SQL upit za dohvaćanje podataka iz tablice Nalaz
+                    string query = "SELECT * FROM Nalazi";
+
+                    if (!string.IsNullOrEmpty(searchTerm))
+                    {
+                        query += " WHERE Pretraga LIKE @searchTerm";
+                    }
+
+                    SqlDataAdapter da = new SqlDataAdapter(query, con);
+                    if (!string.IsNullOrEmpty(searchTerm))
+                    {
+                        da.SelectCommand.Parameters.AddWithValue("@searchTerm", "%" + searchTerm + "%");
+                    }
+
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            // Poziv funkcije za popunjavanje DataGridView-a s parametrima pretrage
+            FillDataGridView(textBoxSearch.Text);
+        }
+
+        
     }
 }
